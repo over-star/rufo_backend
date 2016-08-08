27cf773343ddd94225b6e6fd6ede5cc5805b6129
rufo <775397252@qq.com>
2016-08-09 01:36:14 +0800

    fixed UI

--- resources/views/background/posts/index.blade.php
+++ resources/views/background/posts/index.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-        <h1 class="pull-left">Posts</h1>
+        <div class="page-header">
+                <h1>
+                        用户管理
+                        <small>
+                                <i class="icon-double-angle-right"></i>
+                                用户列表
+                        </small>
+                </h1>
+        </div>
         <a class="btn btn-primary pull-right" style="margin-bottom: 10px" href="{!! route('background.posts.create') !!}">添加</a>
 
         <div class="clearfix"></div>
